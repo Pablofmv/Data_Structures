a10fa70bf2bd8d4b81dffafe6e54c3effce1d9cb
Pablo Mendoza <pablo_fmv@hotmail.com>
2025-04-26 10:01:10 -0400

    Added files to DataStructures

---
+++ Module1/push_back5.py
@@ -0,0 +1,48 @@
+class Node:
+
+    def __init__(self, key):
+        self.key = key
+        self.next = None
+
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+    
+    def pushFront(self,key):
+
+        new_node = Node(key)
+        new_node.next = self.head
+        self.head = new_node
+
+        if new_node.next is None:
+            self.tail = new_node
+    
+    def pushBack(self,key):
+
+        new_node = Node(key)
+
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+    
+    def printList(self):
+
+        current = self.head
+        while current:
+            print(current.key, end = "->")
+            current = current.next
+        print("None")
+
+
+if __name__ == "__main__":
+    ll = LinkedList()
+    ll.pushFront(2)
+    ll.pushFront(1)
+    ll.pushBack(3)
+    ll.pushFront(0)
+    ll.printList()
