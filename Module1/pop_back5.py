26c9e194c7c706cf0dfff1895e774b3e485fb894
Pablo Mendoza <pablo_fmv@hotmail.com>
2025-04-27 11:35:33 -0400

    Added files to DataStructures

---
+++ Module1/pop_back5.py
@@ -0,0 +1,69 @@
+class Node:
+
+    def __init__(self, key):
+        self.key = key
+        self.next = None
+
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def pushFront(self, key):
+
+        new_node = Node(key)
+        new_node.next = self.head
+        self.head = new_node
+
+        if new_node.next == None:
+            self.tail = new_node
+    
+    def pushBack(self,key):
+
+        new_node = Node(key)
+
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+    
+    def popBack(self):
+
+        p = self.head
+
+        if p.next.next is not None:
+            p = p.next
+        p.next = None
+        self.tail = p
+    
+    def popFront(self):
+
+        if self.head is None:
+            raise Exception("Empty List")
+        
+        if self.head == self.tail:
+            self.head = None
+            self.tail = None
+        else:
+            self.head = self.head.next
+    
+    def printList(self):
+        current = self.head
+        while current:
+            print(current.key, end = "->")
+            current = current.next
+        print("None")
+
+if __name__ == "__main__":
+    ll = LinkedList()
+    ll.pushFront(2)
+    ll.pushFront(1)
+    ll.pushBack(3)
+    ll.printList()
+    ll.popBack()
+    ll.printList()
+    ll.popFront()
+    ll.printList()            
