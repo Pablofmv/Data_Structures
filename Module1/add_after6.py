e37c9d5822f74bb81bb0e6e2263babfb0cb8fa8b
Pablo Mendoza <pablo_fmv@hotmail.com>
2025-04-30 19:54:10 -0400

    Added files to DataStructures

---
+++ Module1/add_after6.py
@@ -0,0 +1,100 @@
+class Node:
+
+    def __init__(self, key):
+        self.key = key
+        self.next = None
+
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+    
+    def pushFront(self, key):
+
+        new_node = Node(key)
+        new_node.next = self.head
+        self.head = new_node
+
+        if self.tail is None:
+            self.tail = new_node
+    
+    def pushBack(self, key):
+
+        new_node = Node(key)
+
+        if self.tail is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+    
+    def popBack(self):
+
+        if self.head is None:
+            raise Exception("Empty List")
+        
+        if self.head == self.tail:
+            self.head = None
+            self.tail = None
+        else:
+            p = self.head
+            while p.next.next is not None:
+                p = p.next
+            p.next = None
+            self.tail = p
+    
+    def popFront(self):
+
+        if self.head is None:
+            raise Exception("Empty List")
+        
+        self.head = self.head.next
+
+        if self.head is None:
+            self.tail = None
+
+    def AddAfter(self, node, key):
+
+        new_node = Node(key)
+        new_node.next = node.next
+        node.next = new_node
+
+        if self.tail == node:
+            self.tail = new_node
+
+    def AddAfterKey(self, target_key, new_key):
+
+        current = self.head
+        while current and current.key != target_key:
+            current = current.next
+        
+        if current:
+            self.AddAfter(current, new_key)
+        else:
+            print(f"Node with key {target_key} not found.")
+    
+    def printList(self):
+
+        current = self.head
+
+        while current:
+            print(current.key, end = "->")
+            current = current.next
+        print("None")
+
+if __name__ == "__main__":
+    ll = LinkedList()
+    ll.pushFront(2)
+    ll.pushFront(1)
+    ll.pushBack(3)
+    ll.printList()
+    ll.popBack()
+    ll.printList()
+    ll.popFront()
+    ll.printList()   
+    ll.AddAfterKey(2,4)
+    ll.printList()
+
+    
