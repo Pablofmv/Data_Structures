abfcfb546b0fc465f0bed6a72de77c3882026c13
Pablo Mendoza <pablo_fmv@hotmail.com>
2025-04-30 13:58:02 -0400

    Added files to DataStructures

---
+++ Module1/add_after4.py
@@ -0,0 +1,78 @@
+class Node:
+
+    def __init__(self, key):
+        self.key = key
+        self.next = None
+
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+    
+    def pushFront(self, key):
+
+        new_node = Node(key)
+        new_node.next = self.head
+        self.head = new_node
+
+        if self.tail is None:
+            self.tail = new_node
+
+    def pushBack(self, key):
+
+        new_node = Node(key)
+
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+    
+    def popBack(self):
+
+        if self.head is None:
+            raise Exception("Empty List")
+        
+        if self.head == self.tail:
+            self.head = None
+            self.tail = None
+        else:
+            p = self.head
+            while p.next.next is not None:
+                p = p.next
+            self.tail = p
+            p.next = None
+    
+    def popFront(self):
+
+        if self.head is None:
+            raise Exception("Empty List")
+        
+        self.head = self.head.next
+
+        if self.head is None:
+            self.tail = None
+    
+    def AddAfterKey(self, target_key, new_key):
+
+        current = self.head
+    
+    def printList(self):
+        current = self.head
+        while current:
+            print(current.key, end = "->")
+            current = current.next
+        print("None")
+
+if __name__ == "__main__":
+
+    ll = LinkedList()
+    ll.pushFront(2)
+    ll.pushFront(1)
+    ll.pushBack(3)
+    ll.printList()
+    ll.popFront()
+    ll.printList()
+    ll.popBack()
