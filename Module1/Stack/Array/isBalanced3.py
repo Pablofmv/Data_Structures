db62e45e7d7e93086f4e5b63b45b4c180656bdd4
Pablo Mendoza <pablo_fmv@hotmail.com>
2025-05-03 13:02:47 -0400

    Added files to DataStructures

--- Module1/Stack/Array/isBalanced3.py
+++ Module1/Stack/Array/isBalanced3.py
@@ -1,21 +1,22 @@
-
 def IsBalanced(str):
 
     stack = []
+
     for char in str:
 
-        if char in ['(','[']:
+        if char in ["(","["]:
             stack.append(char)
         else:
+
             if len(stack) == 0:
                 return False
             
             top = stack.pop()
 
-            if (top == '(' and char != ')') or (top == '[' and char != ']'):
+            if (top == '(' and char != ')') and (top == "[" and char != "]"):
                 return False
-    
+            
     return len(stack) == 0
 
-test = '([])'
-print(IsBalanced(test))
+test_str = "()[]([])"
+print(IsBalanced(test_str))
