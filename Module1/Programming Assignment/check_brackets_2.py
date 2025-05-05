8a064f7bbba93671d4a2f416a09508c2072bdc64
Pablo Mendoza <pablo_fmv@hotmail.com>
2025-05-05 16:14:33 -0400

    Added files to DataStructures

--- Module1/Programming Assignment/check_brackets_2.py
+++ Module1/Programming Assignment/check_brackets_2.py
@@ -11,27 +11,28 @@ def are_matching(left, right):
 
 def find_mismatch(text):
     opening_brackets_stack = []
-
     for i, next in enumerate(text):
-        if next in "([{])]}":
+        if next in "([{]})":
+
             if next in "([{":
-                # Process opening bracket, write your code here
-                opening_brackets_stack.append((next,i))
-            elif next in ")]}":
+                opening_brackets_stack.append((i,next))
+                pass
+
+            if next in ")]}":
                 # Process closing bracket, write your code here
                 if len(opening_brackets_stack) == 0:
                     return i + 1
                 
-                pop_char, pop_pos = opening_brackets_stack.pop()
+                pop_i, pop_next = opening_brackets_stack.pop()
 
-                if (pop_char == "(" and next != ")") or (pop_char == "[" and next != "]") or (pop_char == "{" and next != "}"):
+                if (pop_next == "(" and next !=  ")") or (pop_next == "[" and next !=  "]") or (pop_next == "{" and next !=  "}"):
                     return i + 1
     
     if opening_brackets_stack:
-        _, pos = opening_brackets_stack[0]
+        pos, char = opening_brackets_stack[0]
         return pos + 1
-            
-    return "Success" if len(opening_brackets_stack) == 0 else i + 1
+    
+    return "Success"
 
 
 def main():
