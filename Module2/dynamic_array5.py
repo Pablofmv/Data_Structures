bbadd2a9b4bbb5c2d5e462605357546fd5193d92
Pablo Mendoza <pablo_fmv@hotmail.com>
2025-05-10 15:28:37 -0400

    Added files to DataStructures

---
+++ Module2/dynamic_array5.py
@@ -0,0 +1,68 @@
+class DynamicArray:
+
+    def __init__(self):
+        self.arr = [None] * 1
+        self.size = 0
+        self.capacity = 1
+    
+    def Get(self, i):
+
+        if i < 0 or i > self.size:
+            raise IndexError("Index out of range.")
+        
+        return self.arr[i]
+    
+    def Set(self, i, val):
+
+        if i < 0 or i > self.size:
+            raise IndexError("Index out of range.")
+        
+        self.arr[i] = val
+    
+    def PushBack(self, val):
+
+        if self.capacity == self.size:
+
+            new_arr = [None] * (2 * self.capacity)
+
+            for i in range(0, self.capacity):
+                new_arr[i] = self.arr[i]
+            
+            self.arr = new_arr
+            self.capacity = self.capacity * 2
+        
+        self.arr[self.size] = val
+        self.size += 1
+    
+    def Remove(self, i):
+
+        if i < 0 or i > self.size:
+            return IndexError("Index out of range.")
+        
+        for j in range(i, self.size):
+            self.arr[j] = self.arr[j+1]
+        
+        self.size -= 1
+    
+    def Size(self):
+        return self.size
+
+da = DynamicArray()
+da.PushBack(10)
+da.PushBack(20)
+da.PushBack(30)
+
+print(da.arr)
+
+da.Set(1,25)
+
+print(da.arr)
+
+da.Remove(2)
+
+print(da.arr)
+
+da.Remove(0)
+
+print(da.arr)
+
