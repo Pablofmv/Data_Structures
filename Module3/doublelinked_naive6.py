cab813130e7fa7e4f80692cbb1a3791c8c3f8067
Pablo Mendoza <pablo_fmv@hotmail.com>
2025-05-14 23:07:33 -0400

    Added files to DataStructures

--- Module3/doublelinked_naive6.py
+++ Module3/doublelinked_naive6.py
@@ -8,8 +8,8 @@ class Node:
 class PriorityQueue:
 
     def __init__(self):
-        self.head = 0
-        self.tail = 0
+        self.head = None
+        self.tail = None
 
     def Insert(self, e):
 
@@ -23,3 +23,40 @@ class PriorityQueue:
         current = self.head
         while current is not None and current.key < e:
             current = current.next
+        
+        if current == self.head and current.key > e:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+            return
+        
+        if current is None:
+            new_node.prev = self.tail
+            self.tail.next = new_node
+            self.tail = new_node
+            return
+        
+        new_node.prev = current.prev
+        current.prev.next = new_node
+        new_node.next = current
+        current.prev = new_node
+
+    def PrintPriorityQueue(self):
+
+        current = self.head
+        while current:
+            print(current.key, end = "=>")
+            current = current.next
+        print("None")
+
+pq = PriorityQueue()
+pq.Insert(10)
+pq.Insert(20)
+pq.Insert(30)
+pq.Insert(40)
+
+pq.PrintPriorityQueue()
+
+
+
+
