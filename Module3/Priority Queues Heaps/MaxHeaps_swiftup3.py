74b911c21cf9136e124e91fdff3fb3da4544e255
Pablo Mendoza <pablo_fmv@hotmail.com>
2025-05-23 15:44:31 -0400

    Added files to DataStructures

---
+++ Module3/Priority Queues Heaps/MaxHeaps_swiftup3.py
@@ -0,0 +1,71 @@
+class MaxHeap:
+
+    def __init__(self, maxSize):
+        self.H = [0] * (maxSize + 1)
+        self.size = 0
+        self.maxSize = maxSize
+    
+    def parent(self, i):
+        return i // 2
+    
+    def left(self, i):
+        return i * 2
+    
+    def right(self, i):
+        return i * 2 + 1
+    
+    def sift_up(self, i):
+
+        while i > 1 and self.H[self.parent(i)] < self.H[i]:
+            self.H[self.parent(i)], self.H[i] = self.H[i], self.H[self.parent(i)]
+            i = self.parent(i)
+    
+    def insert(self, p):
+
+        if self.size == self.maxSize:
+            return "Error"
+        
+        self.size += 1
+        self.H[self.size] = p
+        self.sift_up(self.size)
+    
+    def sift_down(self, i):
+
+        maxIndex = i
+
+        l = self.left(i)
+        if l <= self.size and self.H[l] > self.H[maxIndex]:
+            maxIndex = l
+        
+        r = self.right(i)
+        if r <= self.size and self.H[r] > self.H[maxIndex]:
+            maxIndex = r
+        
+        if i != maxIndex:
+            self.H[i], self.H[maxIndex] = self.H[maxIndex], self.H[i]
+            self.sift_down(maxIndex)
+
+    
+    def extract_max(self):
+
+        if self.size < 1:
+            return "Error"
+        
+        max_value = self.H[1]
+        self.H[1] = self.H[self.size]
+        self.size -= 1
+        self.sift_down(1)
+        return max_value
+    
+    def print(self):
+        print(self.H[1:self.size + 1])
+
+mh = MaxHeap(10)
+mh.insert(30)
+mh.insert(10)
+mh.insert(20)
+mh.print()
+mh.insert(40)
+mh.print()
+mh.extract_max()
+mh.print()
