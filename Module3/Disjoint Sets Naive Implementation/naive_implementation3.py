7912af5aa1ce10bea81ca91ee6a7d04c80c21dfc
Pablo Mendoza <pablo_fmv@hotmail.com>
2025-05-30 11:44:57 -0400

    Added files to DS

---
+++ Module3/Disjoint Sets Naive Implementation/naive_implementation3.py
@@ -0,0 +1,40 @@
+class DisjointSet:
+
+    def __init__(self, n):
+        self.smallest = list(range(n+1))
+
+    def make_set(self,i):
+        self.smallest[i] = i
+    
+    def find(self, i):
+        return self.smallest[i]
+    
+    def union(self, i, j):
+
+        i_dj = self.find(i)
+        j_dj = self.find(j)
+
+        if i_dj == j_dj:
+            return
+        
+        new_id = min(i_dj, j_dj)
+
+        for k in range(1,len(self.smallest)):
+            if self.smallest[k] == i_dj or self.smallest[k] == j_dj:
+                self.smallest[k] = new_id
+        
+    def __str__(self):
+        return " ".join(str(x) for x in self.smallest[1:])
+
+if __name__ == "__main__":
+
+    ds = DisjointSet(9)
+
+    for i in range(1,10):
+        ds.make_set(i)
+    
+    print(ds)
+
+    ds.union(2,3)
+
+    print(ds)
