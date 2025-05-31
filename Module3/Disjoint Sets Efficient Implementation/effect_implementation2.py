a4ca49ff4bdbb2d1a524b211d8e740e2fdb61390
Pablo Mendoza <pablo_fmv@hotmail.com>
2025-05-31 11:35:04 -0400

    Added files to DS

---
+++ Module3/Disjoint Sets Efficient Implementation/effect_implementation2.py
@@ -0,0 +1,47 @@
+class DisjointSet:
+
+    def __init__(self, n):
+        self.parent = list(range(n + 1))
+        self.rank = list(range(n + 1))
+    
+    def make_set(self, i):
+        self.parent[i] = i
+    
+    def find(self, i):
+
+        while i != self.parent[i]:
+            i = self.parent[i]
+        
+        return i 
+    
+    def union(self, i, j):
+
+        i_dj = self.find(i)
+        j_dj = self.find(j)
+
+        if self.parent[i_dj] == self.parent[j_dj]:
+            return 
+        
+        if self.parent[i_dj] > self.parent[j_dj]:
+            self.parent[j_dj] = i_dj
+        else:
+            self.parent[i_dj] = j_dj
+            if self.rank[i_dj] == self.rank[j_dj]:
+                self.rank[j_dj] += 1
+    
+    def __str__(self):
+        return " ".join(str(x) for x in self.parent[1:])
+
+if __name__ == "__main__":
+
+    ds = DisjointSet(5)
+    for i in range(1,6):
+        ds.make_set(i)
+    
+    ds.union(1,4)
+    ds.union(4,3)
+    ds.union(3,2)
+    print(ds)
+
+    print(f"Are 2 and 5 connected? {ds.find(1) == ds.find(2)}")
+    
