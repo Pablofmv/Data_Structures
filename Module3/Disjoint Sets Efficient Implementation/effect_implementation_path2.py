e2efc2889060e03d36affc427cae0659f37f69a7
Pablo Mendoza <pablo_fmv@hotmail.com>
2025-06-01 10:40:07 -0400

    Added files to DS

---
+++ Module3/Disjoint Sets Efficient Implementation/effect_implementation_path2.py
@@ -0,0 +1,47 @@
+class DisjoinSet:
+
+    def __init__(self, n):
+        self.parent = list(range(n + 1))
+        self.rank = list(range(n + 1))
+    
+    def make_set(self, i):
+        self.parent[i] = i
+    
+    def find(self, i):
+
+        if i != self.parent[i]:
+            self.parent[i] = self.find(self.parent[i])
+        
+        return self.parent[i]
+    
+    def union(self, i, j):
+
+        i_dj = self.parent[i]
+        j_dj = self.parent[j]
+
+        if i_dj == j_dj:
+            return
+        
+        if self.rank[i_dj] > self.rank[j_dj]:
+            self.parent[j_dj] = i_dj
+        else:
+            self.parent[i_dj] = j_dj
+
+            if self.rank[i_dj] == self.rank[j_dj]:
+                self.rank[j_dj] += 1
+    
+    def __str__(self):
+        return " ".join(str(x) for x in self.parent[1:])
+
+if __name__ == "__main__":
+
+    ds = DisjoinSet(5)
+
+    for i in range(1,6):
+        ds.make_set(i)
+    
+    ds.union(1,4)
+    ds.union(4,3)
+    ds.union(3,2)
+
+    print(ds)
