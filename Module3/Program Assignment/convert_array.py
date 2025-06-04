a4c688d3ff750c8a0054f182d7cc8135aea75423
Pablo Mendoza <pablo_fmv@hotmail.com>
2025-06-04 13:03:49 -0400

    Added files to DS

--- Module3/Program Assignment/convert_array.py
+++ Module3/Program Assignment/convert_array.py
@@ -28,13 +28,28 @@ def build_heap(A):
     n = len(A)
     size = n - 1
 
-    for i in range(n//2, -1, -1):
+    for i in range(n//2, 0, -1):
         sift_down(A, i, size)
     
-    return A
+    return size
+
+def heap_sort(A):
+
+    A = [None] + A[:]
+    size = build_heap(A)
+    print(A)
+
+    for i in range(0,size-1):
+        A[1], A[size] = A[size], A[1]
+        size -= 1
+        sift_down(A,1,size)
+    
+    print(A[1:])
+
+
 
 if __name__ == "__main__":
 
-    A = [1,2,3]
+    A = [100,1,2,3,4]
     heap = build_heap(A)
-    print(heap)
+    heap_sort(A)
