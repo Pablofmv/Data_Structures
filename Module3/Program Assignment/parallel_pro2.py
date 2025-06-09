6b0e085d984352a86f0a81b62e94e82eea4a5155
Pablo Mendoza <pablo_fmv@hotmail.com>
2025-06-09 10:11:04 -0400

    Added files to DS

--- Module3/Program Assignment/parallel_pro2.py
+++ Module3/Program Assignment/parallel_pro2.py
@@ -10,4 +10,39 @@ def right_child(i):
 def sift_down(A, i, size):
 
     minIndex = i
+    left = left_child(minIndex)
+    if left <= size and A[left] < A[minIndex]:
+        minIndex = left
     
+    right = right_child(minIndex)
+    if right <= size and A[right] < A[minIndex]:
+        minIndex = right
+    
+    if i != minIndex:
+        A[minIndex], A[i] = A[i], A[minIndex]
+        sift_down(A, minIndex, size)
+
+def build_heap(n):
+    return [None] + [(0,i) for i in range(0,n)]
+
+def assign_jobs(n,m, jobs):
+    
+    threads_list = build_heap(n)
+    
+    for duration in jobs:
+        
+        time_free, thread = threads_list[1]
+
+        print(thread, time_free)
+
+        threads_list[1] = (time_free + duration, thread)
+
+        sift_down(threads_list, 1, n + 1)
+    
+
+
+if __name__ == "__main__":
+    n, m = map(int, input().split())
+    jobs = list(map(int, input().split()))
+
+    assign_jobs(n,m,jobs)
