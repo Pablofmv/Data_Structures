42242d7a1f34f7feca98ede0098fe5a9d7b8430f
Pablo Mendoza <pablo_fmv@hotmail.com>
2025-06-11 06:34:31 -0400

    Added files to DS

--- Module3/Program Assignment/parallel_pro4.py
+++ Module3/Program Assignment/parallel_pro4.py
@@ -1,4 +1,26 @@
 
+def parents(i):
+    return i // 2
+
+def left_child(i):
+    return i * 2
+
+def right_child(i):
+    return i * 2 + 1
+
+
+
+
+
+def thread_heap(n):
+    return [None] + [(0,i) for i in range(n)]
+
+def assign_jobs(n, m, jobs):
+
+
 
 if __name__ == "__main__":
     n, m = input().split()
+    jobs = list(map(int, input().split()))
+
+    assign_jobs(n,m,jobs)
