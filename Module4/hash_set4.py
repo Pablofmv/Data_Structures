71591534eaa97b202294550ea0a731eebf63922c
Pablo Mendoza <pablo_fmv@hotmail.com>
2025-06-19 19:50:31 -0700

    Added files to DS

--- Module4/hash_set4.py
+++ Module4/hash_set4.py
@@ -1,5 +1,5 @@
 
-def hash_function(m, key):
+def hash_function(key, m):
     return hash(key) % m
 
 
@@ -9,6 +9,50 @@ class HashSet:
         self.m = m
         self.chains = [[] for i in range(0,m)]
     
-    def find(self, m):
+    def find(self, key):
 
+        index = hash_function(key, self.m)
+        chain = self.chains[index]
+
+        if key in chain:
+            return True
         
+        return False
+    
+    def add(self, key):
+
+        index = hash_function(key, self.m)
+        chain = self.chains[index]
+
+        if key in chain:
+            return
+        
+        chain.append(key)
+    
+    def remove(self, key):
+
+        index = hash_function(key, self.m)
+        chain = self.chains[index]
+
+        if not self.find(key):
+            return 
+        
+        chain.remove(key)
+    
+
+if __name__ == "__main__":
+
+    hs = HashSet(5)
+    hs.add("Pablo")
+    hs.add("Carlos")
+
+    print(hs.chains)
+
+    hs.add("Pablo")
+    
+    print(hs.chains)
+
+    hs.remove("Pablo")
+
+    print(hs.chains)
+
