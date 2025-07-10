6b086c327bb586eb2d6356d5e8dedda4bc09b61f
Pablo Mendoza <pablo_fmv@hotmail.com>
2025-07-10 10:20:29 -0400

    Added files to DS

--- Module4/Search Substring/RK_Precom3.py
+++ Module4/Search Substring/RK_Precom3.py
@@ -6,24 +6,23 @@ def poly_hash(S, p, x):
         hash_value = (hash_value * x + ord(c)) % p
     return hash_value
 
-def pre_comput_hashes(T, pattern_len, p, x):
-
+def pre_compute_hashes(T, pattern_len, p, x):
     n = len(T)
     H = [0] * (n - pattern_len + 1)
-    S =  T[n - pattern_len:]
-    H[-1] = poly_hash(S,p,x)
+    S = T[n - pattern_len:]
+    H[-1] = poly_hash(S, p , x)
 
     y = 1
     for i in range(0, pattern_len):
         y = (y * x) % p
     
     for i in reversed(range(n - pattern_len)):
-        pre_hash = (x * H[i + 1] + ord(T[i]) - y *ord(T[i + pattern_len])) % p
+        pre_hash = (x * H[i+1] + ord(T[i]) - y * ord(T[i + pattern_len])) % p
         H[i] = (pre_hash + p) % p
     
     return H
 
-def are_equal(S1,S2):
+def are_equal(S1, S2):
     return S1 == S2
 
 def rabin_karp(T, P):
@@ -33,12 +32,14 @@ def rabin_karp(T, P):
     result = []
 
     p_hash = poly_hash(P, p, x)
-    H = pre_comput_hashes(T, len(P), p, x)
+
+    H = pre_compute_hashes(T, len(P), p, x)
 
     for i in range(0, len(T) - len(P) + 1):
         if H[i] != p_hash:
             continue
-        if are_equal(T[i:i+len(P)], P):
+
+        if are_equal(T[i:i+len(P)],P):
             result.append(i)
     
     return result
@@ -47,4 +48,4 @@ if __name__ == "__main__":
 
     text = "abracadabra"
     pattern = "abra"
-    print(rabin_karp(text, pattern)) 
+    print(rabin_karp(text, pattern))
