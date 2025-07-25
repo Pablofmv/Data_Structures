85935fa2135e1bc0cf2a2d17b356e647fbce0cc1
Pablo Mendoza <pablo_fmv@hotmail.com>
2025-07-25 10:18:41 -0400

    Added files to DS

---
+++ Module5/Binary_search_three11.py
@@ -0,0 +1,64 @@
+class Node:
+
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+        self.parent = None
+
+def find(k, root):
+
+    if root is None:
+        return None
+    
+    if root.key == k:
+        return root
+    elif k < root.key:
+        return find(k, root.left)
+    else:
+        return find(k, root.right)
+
+def next_node(N):
+    if N.right:
+        return left_descendant(N.right)
+    else:
+        return right_ancestor(N)
+
+def left_descendant(N):
+    while N.left:
+        N = N.left
+    return N
+
+def right_ancestor(N):
+    while N.parent and N.parent.right == N:
+        N = N.parent
+    return N.parent
+
+def connect(parent, child, is_left = True):
+    if is_left:
+        parent.left = child
+    else:
+        parent.right = child
+    
+    if child:
+        child.parent = parent
+
+root = Node(7)
+n4 = Node(4)
+n13 = Node(13)
+n1 = Node(1)
+n6 = Node(6)
+n15 = Node(15)
+n14 = Node(14)
+
+connect(root, n4, True)
+connect(root, n13, False)
+connect(n4, n1, True)
+connect(n4, n6, False)
+connect(n13, n15, False)
+connect(n15, n14, True)
+
+target = find(14,root)
+print(target.key if target else "None")
+succ = next_node(target)
+print(succ.key if succ else "None")
